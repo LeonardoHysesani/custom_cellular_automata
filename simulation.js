6028be3c470e84478292329e1d12668ac201d1cd
LeonardoHysesani <lhysesani@gmail.com>
2024-02-20 05:21:34 +0200

    Separated rule generation logic

--- simulation.js
+++ simulation.js
@@ -83,7 +83,6 @@ class Neighbourhood {
 let simulationSpeed = 5;
 let gridSize = canvasSize/cellSize;
 let grid= getEmptyGrid();
-let ruleString = generateRuleString();
 let simulationEnabled = true;
 /**
  * ID of current interval running the simulation.
@@ -91,38 +90,6 @@ let simulationEnabled = true;
  * @type {number}
  */
 let intervalId = -1;
-
-
-/**
- *
- * @returns {string} Binary representation of generated ruleset.
- */
-function generateRuleString() {
-    let rules = "";
-    for (let i = 0; i < 512; i++) {
-        let rule = undefined;
-        const neighbourhood = new Neighbourhood().fromBinaryString(Number(i).toString(2).padStart(9, "0"));
-        if (neighbourhood.mainCell() === 1) {
-            if (neighbourhood.liveNeighbours() < 2 || 4 <= neighbourhood.liveNeighbours()) {
-                rule = "0";
-            }
-            else {
-                rule = "1";
-            }
-        }
-        if (neighbourhood.mainCell() === 0) {
-            if (neighbourhood.liveNeighbours() === 3) {
-                rule = "1";
-            }
-            else {
-                rule = "0";
-            }
-        }
-        rules += rule;
-    }
-    console.log(rules);
-    return rules;
-}
 
 /**
  * Clears current interval and creates a new one with the given speed.
